054edc87b0d361033a90268c643e815bd92b6ce5
Gabriel Bastos <ggbstos@gmail.com>
2022-05-20 18:29:53 -0300

    Acrescentados arquivo externo para estilização e pequenas alterações no HTML

--- app.js
+++ app.js
@@ -1,33 +1,38 @@
 function ageConversor(ageInDays) {
-    let age = ageInDays;
-
     let daysToYears = age / 365;
+    let yearsToMonths;
+    let monthsToDays;
     const dayToYearsInt = Math.trunc(daysToYears);
+    const yearsToMonthsInt = Math.trunc(yearsToMonths);
+    const monthsToDaysInt = Math.ceil(monthsToDays);
+
     console.log("Ano(s): " + dayToYearsInt);
 
-    let yearsToMonths = (daysToYears - Math.trunc(daysToYears)) * 12;
-    const yearsToMonthsInt = Math.trunc(yearsToMonths);
-    age == 30 ? console.log("Mes(es): " + 1) : console.log("Mes(es): " + yearsToMonthsInt);
+    yearsToMonths = (daysToYears - Math.trunc(daysToYears)) * 12;
+    ageInDays === 30 
+    ? console.log("Mes(es): " + 1) 
+    : console.log("Mes(es): " + yearsToMonthsInt);
 
-    let monthsToDays = (yearsToMonths - Math.trunc(yearsToMonths)) * 30;
-    const monthsToDaysInt = Math.ceil(monthsToDays);
-    age == 30 ? console.log("Dia(s): " + 0) : console.log("Dia(s): " + monthsToDaysInt);
+    monthsToDays = (yearsToMonths - Math.trunc(yearsToMonths)) * 30;
+    ageInDays === 30 
+    ? console.log("Dia(s): " + 0) 
+    : console.log("Dia(s): " + monthsToDaysInt);
 }
 
 let ageInDays = 30;
 ageConversor(ageInDays); // Ano(s): 0; Mes(es): 1; Dia(s): 0
 
-/* Pensar numa forma de considerar o decorrer do tempo nesta função (das 7h às 7h, passaram-se 24h, mas a função, no momento,
+/* FIXME: Pensar numa forma de considerar o decorrer do tempo nesta função (das 7h às 7h, passaram-se 24h, mas a função, no momento,
  * não suporta essa ideia, devido à lógica que nela apliquei) */
 function gameTime(hoursMinutesValues){
     let gameDurationHour = hoursMinutesValues[2] - hoursMinutesValues[0];
     let gameDurationMinute = hoursMinutesValues[3] - hoursMinutesValues[1];
     let gameDuration = "O jogo durou " + gameDurationHour + " hora(s) e " + gameDurationMinute + " minuto(s)";
-    return gameDuration;
+    return console.log(gameDuration);
 }
 
 let x = [7,7,7,7]; // Output deve ser 24h, preciso aprefeiçoar o código para que isso aconteça
-console.log(gameTime(x));
+gameTime(x);
 
 // Tabuada do 5
 function tabuadaDo5(){
@@ -40,13 +45,13 @@ function tabuadaDo5(){
 
 tabuadaDo5();
 
-// Quadrado de pares
+// FIXME: Quadrado de pares
 function pairSquare(x){
     let num = x
     if (num <= 5 || num > 2000){
         console.log("Esta não é uma entrada válida.");
     } else {
-        for (let i = 2; i <= num; i = i + 2) {
+        for (let i = 2; i <= num; i += 2) {
             let square = num * num;
             console.log(square);
         }
