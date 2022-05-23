5d9d833bf609d1958d155aa21e4c493c982be3ac
Gabriel Bastos <ggbstos@gmail.com>
2022-05-23 20:14:43 -0300

    Explorando funções e template literals

--- JS/functions.js
+++ JS/functions.js
@@ -1,5 +1,5 @@
 // Funções
-// A principal função das funções é reutilizá-las (reutilizar pedaços de códigos caralho)
+// A principal função das funções é reutilizá-las (reutilizar pedaços de códigos caralho!!)
 
 let numeroA = 2
 let numeroB = 1
@@ -7,7 +7,7 @@ let numeroB = 1
 let soma = numeroA + numeroB
 console.log(soma)
 
-function somar(numeroA, numeroB){ //Corpo da função: entre parênteses estão os parâmetros de entrada (elementos que serão somados)
+function somar(numeroA, numeroB) { //Corpo da função: entre parênteses estão os parâmetros de entrada (elementos que serão somados)
     let soma = numeroA + numeroB // Nos curly brackets, está a lógica da soma (somar dessa forma é recurso da linguagem)
     return soma
 }
@@ -17,9 +17,19 @@ console.log(somar(1,5)) // 6
 let _numeroA = 4
 let _numeroB = 6
 
-function mediana(_numeroA, _numeroB){
+function mediana(_numeroA, _numeroB) {
     let media = somar(_numeroA, _numeroB)/2
     return media
 }
 
 console.log(mediana(4,6)) // 5
+
+function ola(nome) {
+  console.log(`Olá, ${nome}!`); 
+    // Utilizando template literals (${variável}) é possível integrar variáveis em strings
+}
+
+ola("Gabriel") 
+  // Não é necessário utilizar console.log para invocar a função neste caso, 
+  // pois ela não possui return. Ou seja, não há uma variável sendo retornada
+  // como "resultado da função", do modo que ocorre nas funções acima.
